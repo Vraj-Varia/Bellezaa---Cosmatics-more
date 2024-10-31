19b11b2de797964bc06cb7eea48ce9363c846a21
Vraj Varia <vrajvaria10@gmail.com>
2024-10-31 17:16:19 +0530

    advertisement created

--- src/Support/Support.jsx
+++ src/Support/Support.jsx
@@ -5,38 +5,38 @@ import SmallCard from './SmallCard'
 const Support = () => {
   return (
     <>
-    <div className='adjustEverything'>
-    <div className="divider"></div>
-      <div className='Support'>
+      <div className='adjustEverything'>
+        <div className="divider"></div>
+        <div className='Support'>
           <h1 className='title'>Support</h1>
           <div className="separator"></div>
           <form action="#" method="get">
-              <input type="text" name="name" id="name" placeholder='Name'/>
-              <input type="text" name="phone" id="phone" placeholder='Phone'/>
-              <input type="email" name="email" id="email" placeholder='Email'/>
-              <textarea name="message" id="message" placeholder='Message...'></textarea>
-              <button>Contact us</button>
+            <input type="text" name="name" id="name" placeholder='Name' />
+            <input type="text" name="phone" id="phone" placeholder='Phone' />
+            <input type="email" name="email" id="email" placeholder='Email' />
+            <textarea name="message" id="message" placeholder='Message...'></textarea>
+            <button>Contact us</button>
           </form>
-      </div>
-      <div className="divider"></div>
-      <div className="needHelp">
-        <div className="heading">
-          <h1>Need a Little Guidance?</h1>
-          <p>Check out what's popular now.</p>
         </div>
-        <div className="cards">
-          <SmallCard title="Blush"/>
-          <SmallCard title="Serums"/>
-          <SmallCard title="Trending on social"/>
-          <SmallCard title="Sale"/>
-          <SmallCard title="Clean + Planet aware of Sephora"/>
-          <SmallCard title="Black Owned Brands"/>
-          <SmallCard title="Foundation"/>
-          <SmallCard title="Gifts"/>
+        <div className="divider"></div>
+        <div className="needHelp">
+          <div className="heading">
+            <h1>Need a Little Guidance?</h1>
+            <p>Check out what's popular now.</p>
+          </div>
+          <div className="cards">
+            <SmallCard title="Blush" />
+            <SmallCard title="Serums" />
+            <SmallCard title="Trending on social" />
+            <SmallCard title="Sale" />
+            <SmallCard title="Clean + Planet aware of Sephora" />
+            <SmallCard title="Black Owned Brands" />
+            <SmallCard title="Foundation" />
+            <SmallCard title="Gifts" />
+          </div>
         </div>
+        <div className="divider"></div>
       </div>
-      <div className="divider"></div>
-    </div>
     </>
   )
 }
